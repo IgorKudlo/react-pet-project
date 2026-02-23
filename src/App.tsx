951ff498a9315ae508f01bd27bdbdaf1b9a00073
igor.kudlo <igoreklida@gmail.com>
2026-02-23 09:16:47 +0300

    helper classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route, Link } from "react-router";
 import { Suspense } from 'react';
 import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { useTheme } from './theme/useTheme';
 
 function App() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
